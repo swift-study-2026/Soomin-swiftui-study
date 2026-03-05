c73b1f29ea9772cefed8ff565aff2fcab7b730c7
sumchive <haon0319@naver.com>
2026-03-05 21:38:18 +0900

    [Init] 실습 초기 세팅

---
+++ swiftui-week1/swiftui-week1/swiftui_week1App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_week1App.swift
+//  swiftui-week1
+//
+//  Created by mandoo on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_week1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
