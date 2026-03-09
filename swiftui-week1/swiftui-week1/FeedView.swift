6604620de7894df6ae2a7dfe05ffb239188eb2af
sumchive <haon0319@naver.com>
2026-03-09 15:33:58 +0900

    [chore] Feed 모델 추가 및 ImageResource로 변경

--- swiftui-week1/swiftui-week1/FeedView.swift
+++ swiftui-week1/swiftui-week1/FeedView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+struct Feed: Identifiable {
+    let id: UUID = UUID()
+    let image: ImageResource
+}
+
 struct FeedView: View {
     let columns: [GridItem] = [
         GridItem(.flexible(), spacing: 5),
@@ -14,12 +19,26 @@ struct FeedView: View {
         GridItem(.flexible(), spacing: 5)
     ]
     
-    let imageNames: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+    let feeds: [Feed] = [
+        Feed(image: .feed1),
+        Feed(image: .feed2),
+        Feed(image: .feed3),
+        Feed(image: .feed4),
+        Feed(image: .feed5),
+        Feed(image: .feed6),
+        Feed(image: .feed7),
+        Feed(image: .feed8),
+        Feed(image: .feed9),
+        Feed(image: .feed10),
+        Feed(image: .feed11),
+        Feed(image: .feed12),
+        Feed(image: .feed13),
+    ]
     
     var body: some View {
         LazyVGrid(columns: columns, spacing: 5) {
-            ForEach(imageNames, id: \.self) { imageName in
-                Image(imageName)
+            ForEach(feeds) { feed in
+                Image(feed.image)
                     .resizable()
                     .aspectRatio(1, contentMode: .fill)
             }
