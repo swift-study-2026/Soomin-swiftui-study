5bc07ef92dcd9e662ec29c09d1d23e4c5bd46548
sumchive <haon0319@naver.com>
2026-03-23 01:13:04 +0900

    [feat] User 모델 구현

---
+++ swiftui-week1/swiftui-week1/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  swiftui-week1
+//
+//  Created by mandoo on 3/23/26.
+//
+
+import Observation
+
+@Observable
+final class User {
+    var name: String
+    var bio: String
+    var isPrivate: Bool
+    var isLoggedIn: Bool
+
+    init(name: String = "알 수 업서요", bio: String = "어떤 사람..?", isPrivate: Bool = false, isLoggedIn: Bool = false) {
+        self.name = name
+        self.bio = bio
+        self.isPrivate = isPrivate
+        self.isLoggedIn = isLoggedIn
+    }
+}
